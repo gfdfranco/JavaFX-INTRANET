abc7580fe56a78c7b6fca3be77943c1e5b02c19e
Franco (Choco) <gfdgeras@gmail.com>
2015-04-04 21:36:16 -0600

    Funciona Cambio de Main
    Se puede cambiar de main

--- src/ch/makery/address/MainProfesor.java
+++ src/ch/makery/address/MainProfesor.java
@@ -7,17 +7,16 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class MainProfesor extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-    @SuppressWarnings("unused")
-	private MainAlumno alumno;
 
     @Override
     public void start(Stage primaryStage) {
@@ -32,36 +31,31 @@ public class MainApp extends Application {
     /**
      * Initializes the root layout.
      */
+   
     public void initRootLayout() {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            loader.setLocation(MainProfesor.class.getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
-            
-          
-            
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        }
     }
-
+ 
    
     public void showPersonOverview() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(MainProfesor.class.getResource("view/Profesor.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
-            
+
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
         } catch (IOException e) {
@@ -69,18 +63,6 @@ public class MainApp extends Application {
         }
     }
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-    	  Application app2 = new MainProfesor(); 
-          Stage anotherStage = new Stage();
-          try {
-			app2.start(anotherStage);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-    }
     
     public Stage getPrimaryStage() {
         return primaryStage;
